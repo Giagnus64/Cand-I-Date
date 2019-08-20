48656b2eba52ec2aa52b41d37bb857ab8d807597
Gianfranco Nuschese <gianfranconuschese@gmail.com>
2019-08-20 17:35:42 -0400

    Finished basic validations

--- app/models/follow.rb
+++ app/models/follow.rb
@@ -1,4 +1,7 @@
 class Follow < ApplicationRecord
   belongs_to :user
   belongs_to :politician
+  validates :match, presence: true
+  #custom validation to make sure no politician can be on a user
+
 end
--- app/models/politician.rb
+++ app/models/politician.rb
@@ -3,4 +3,10 @@ class Politician < ApplicationRecord
     has_many :issues, through: :stances
     has_many :follows
     has_many :users, through: :follows
+    validates :first_name, :last_name, :state, :img_url, :party, :chamber, presence: true
+
+    #custome method to determine if a user has seen this politician>?
+    #concat first_name and Last_Name
+    #State Array?
+    #Party Array?
 end
